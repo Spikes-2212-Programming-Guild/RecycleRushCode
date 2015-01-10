6f4e70054b08e9baa08cbf672c4db4f6dcb81334
ThinkRedstone <ittai11@gmail.com>
2015-01-10 15:58:11 +0200

    added JoystickMap with driverJ, and implemented getDriverX in OI

---
+++ org/usfirst/frc/team2212/robot/JoystickMap.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc.team2212.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ *
+ * @author ThinkRedstone
+ */
+public class JoystickMap {
+    protected Joystick driverJ = new Joystick(0);
+}
--- org/usfirst/frc/team2212/robot/OI.java
+++ org/usfirst/frc/team2212/robot/OI.java
@@ -7,7 +7,7 @@ import org.usfirst.frc.team2212.robot.commands.ExampleCommand;
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
-public class OI {
+public class OI extends JoystickMap{
     //// CREATING BUTTONS
     // One type of button is a joystick button which is any button on a joystick.
     // You create one by telling it which joystick it's on and which button
@@ -34,5 +34,11 @@ public class OI {
     // Start the command when the button is released  and let it run the command
     // until it is finished as determined by it's isFinished method.
     // button.whenReleased(new ExampleCommand());
+    public double getDriverY(){
+        return driverJ.getY();
+    }
+    public double getDriverX(){
+    return driverJ.getX();
+}
 }
 
