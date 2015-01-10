9aa43852986060125b41fa86b3625b78c4a2701d
ThinkRedstone <ittai11@gmail.com>
2015-01-10 16:20:14 +0200

    added and implemented Sideways

---
+++ org/usfirst/frc/team2212/robot/commands/driving/Sideways.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc.team2212.robot.commands.driving;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team2212.robot.Robot;
+
+/**
+ *
+ * @author ThinkRedstone
+ */
+public class Sideways extends Command{
+    
+    public Sideways() {
+        requires(Robot.driveTrain);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        Robot.driveTrain.sideways(Robot.oi.getDriverX());
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+        Robot.driveTrain.sideways(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+        end();
+    }
+}
