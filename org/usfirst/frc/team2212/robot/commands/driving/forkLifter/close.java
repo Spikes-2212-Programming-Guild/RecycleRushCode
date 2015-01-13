4d84271921abdb39b91add0c28bced9a9de6edba
ThinkRedstone <ittai11@gmail.com>
2015-01-13 08:43:57 +0200

    added close

---
+++ org/usfirst/frc/team2212/robot/commands/driving/forkLifter/close.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc.team2212.robot.commands.driving.forkLifter;
+
+import edu.wpi.first.wpilibj.command.Command;
+import static org.usfirst.frc.team2212.robot.Robot.fork;
+
+/**
+ *
+ * @author ThinkRedstone
+ */
+public class close extends Command{
+    
+    public close() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        requires(fork);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        fork.close();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return fork.isClosed();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+        fork.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+        end();
+    }
+}
