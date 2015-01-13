cbed3ea04a04ec40fa43d6720c555ba1d34c20e9
ThinkRedstone <ittai11@gmail.com>
2015-01-13 18:34:32 +0200

    added gettters for encoders in drivetrain and ENCODERS_TICKS_IN_FULL_TURN

--- org/usfirst/frc/team2212/robot/RobotMap.java
+++ org/usfirst/frc/team2212/robot/RobotMap.java
@@ -19,6 +19,7 @@ public class RobotMap {
     public static final int SIDEWAYS_ENCODER_1_PORT = 0;
     public static final int SIDEWAYS_ENCODER_2_PORT = 0;
     public static final double WHEEL_DIAMETER = 0;
+    public static final int ENCODER_TICKS_IN_FULL_TURN = 1;
 
     public static final int FORK_LOCK_PORT = 0;
     public static final int FORK_OPEN_PORT = 0;
@@ -37,4 +38,5 @@ public class RobotMap {
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
+    
 }
--- org/usfirst/frc/team2212/robot/subsystems/DriveTrain.java
+++ org/usfirst/frc/team2212/robot/subsystems/DriveTrain.java
@@ -9,6 +9,7 @@ import components.Gearbox;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team2212.robot.RobotMap;
 
 /**
  *
@@ -32,7 +33,7 @@ public class DriveTrain extends Subsystem {
     }
 
     public DriveTrain(int leftForward, int leftBackwards, int rightForward, int rightBackwards, int middleFront, int middleRear, int forwardEncoderPort1, int forwardEncoderPort2, int sidewaysEncoderPort1, int sidewaysEncoderPort2, double wheelDiameter) {
-        this(new Gearbox(leftForward, leftBackwards), new Gearbox(rightForward, rightBackwards), new VictorSP(middleFront), new VictorSP(middleRear), new Encoder(forwardEncoderPort1, forwardEncoderPort2),new Encoder(sidewaysEncoderPort1, sidewaysEncoderPort2), wheelDiameter);
+        this(new Gearbox(leftForward, leftBackwards), new Gearbox(rightForward, rightBackwards), new VictorSP(middleFront), new VictorSP(middleRear), new Encoder(forwardEncoderPort1, forwardEncoderPort2), new Encoder(sidewaysEncoderPort1, sidewaysEncoderPort2), wheelDiameter);
     }
 
     public void forward(double speed) {
@@ -57,6 +58,14 @@ public class DriveTrain extends Subsystem {
     public void freeMovement(double forwardSpeed, double sidewaysSpeed) {
         forward(forwardSpeed);
         sideways(sidewaysSpeed);
+    }
+
+    public double forwardGet() {
+        return wheelDiameter * Math.PI * (forward.get() / RobotMap.ENCODER_TICKS_IN_FULL_TURN);
+    }
+
+    public double sidewaysGet() {
+        return wheelDiameter * Math.PI * (sideways.get() / RobotMap.ENCODER_TICKS_IN_FULL_TURN);
     }
 
     // Put methods for controlling this subsystem
