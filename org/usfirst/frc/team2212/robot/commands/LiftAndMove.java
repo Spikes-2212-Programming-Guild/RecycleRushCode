bff1949561d49c0b78f01075a545dfe509b17336
ThinkRedstone <ittai11@gmail.com>
2015-01-20 18:22:38 +0200

    Now using PIDMove

--- org/usfirst/frc/team2212/robot/commands/LiftAndMove.java
+++ org/usfirst/frc/team2212/robot/commands/LiftAndMove.java
@@ -7,9 +7,9 @@ package org.usfirst.frc.team2212.robot.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.usfirst.frc.team2212.robot.RobotMap;
+import org.usfirst.frc.team2212.robot.commands.PID.PIDMove;
 import org.usfirst.frc.team2212.robot.commands.PID.PIDSideways;
 import org.usfirst.frc.team2212.robot.commands.forkLifter.LiftALittle;
-import org.usfirst.frc.team2212.robot.commands.forkLifter.Move;
 
 /**
  *
@@ -19,8 +19,7 @@ public class LiftAndMove extends CommandGroup {
 
     public LiftAndMove() {
         addSequential(new LiftALittle());
-//        TODO use PIDelevator
-        addParallel(new Move());
+        addParallel(new PIDMove(RobotMap.ONE_TOTE_DEST, RobotMap.ONE_TOTE_KP, RobotMap.ONE_TOTE_KI, RobotMap.ONE_TOTE_KD, RobotMap.ONE_TOTE_DT, RobotMap.ONE_TOTE_THRESHOLD));
         addSequential(new PIDSideways(RobotMap.AUTO_SIDEWAYS_DEST, RobotMap.AUTO_SIDEWAYS_KP, RobotMap.AUTO_SIDEWAYS_KI, RobotMap.AUTO_SIDEWAYS_KD, RobotMap.AUTO_SIDEWAYS_DT, RobotMap.AUTO_SIDEWAYS_THRESHOLD));
     }
 
