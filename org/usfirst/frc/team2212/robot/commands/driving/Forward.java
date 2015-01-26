563b6bef98201e31b25c1709c27cc61a892082ba
Thinkredstone <ittai11@gmail.com>
2015-01-26 20:19:46 +0200

    Fixed EVERYTHING (not code, git bugs)

--- org/usfirst/frc/team2212/robot/commands/driving/Forward.java
+++ org/usfirst/frc/team2212/robot/commands/driving/Forward.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.usfirst.frc.team2212.robot.commands.Driving;
+package org.usfirst.frc.team2212.robot.commands.driving;
 
 import edu.wpi.first.wpilibj.command.Command;
 import static org.usfirst.frc.team2212.robot.Robot.driveTrain;
