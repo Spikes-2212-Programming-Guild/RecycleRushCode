2d13019a71b646af87772f7fb5043dc05b73f557
dvir42 <dviros7@gmail.com>
2015-02-06 16:44:16 +0200

    Removed Examples

--- src/org/usfirst/frc/team2212/robot/Robot.java
+++ src/org/usfirst/frc/team2212/robot/Robot.java
@@ -1,14 +1,14 @@
 package org.usfirst.frc.team2212.robot;
 
+import org.usfirst.frc.team2212.robot.commands.AutonomousCommand;
+import org.usfirst.frc.team2212.robot.subsystems.DriveTrain;
+import org.usfirst.frc.team2212.robot.subsystems.Fork;
+import org.usfirst.frc.team2212.robot.subsystems.Lifter;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import org.usfirst.frc.team2212.robot.commands.ExampleCommand;
-import org.usfirst.frc.team2212.robot.subsystems.DriveTrain;
-import org.usfirst.frc.team2212.robot.subsystems.ExampleSubsystem;
-import org.usfirst.frc.team2212.robot.subsystems.Fork;
-import org.usfirst.frc.team2212.robot.subsystems.Lifter;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -19,75 +19,95 @@ import org.usfirst.frc.team2212.robot.subsystems.Lifter;
  */
 public class Robot extends IterativeRobot {
 
-    public static final DriveTrain driveTrain = new DriveTrain(RobotMap.LEFT_FORWARD_VICTOR_PORT, RobotMap.LEFT_BACKWARDS_VICTOR_PORT, RobotMap.RIGHT_FORWARD_VICTOR_PORT, RobotMap.RIGHT_BACKWARDS_VICTOR_PORT, RobotMap.MIDDLE_FRONT_VICTOR_PORT, RobotMap.MIDDLE_BACKWARDS_VICTOR_PORT,RobotMap.FORWARD_ENCODER_1_PORT, RobotMap.FORWARD_ENCODE2_2_PORT, RobotMap.SIDEWAYS_ENCODER_1_PORT, RobotMap.SIDEWAYS_ENCODER_2_PORT, RobotMap.WHEEL_DIAMETER);
+	public static final DriveTrain driveTrain = new DriveTrain(
+			RobotMap.LEFT_FORWARD_VICTOR_PORT,
+			RobotMap.LEFT_BACKWARDS_VICTOR_PORT,
+			RobotMap.RIGHT_FORWARD_VICTOR_PORT,
+			RobotMap.RIGHT_BACKWARDS_VICTOR_PORT,
+			RobotMap.MIDDLE_FRONT_VICTOR_PORT,
+			RobotMap.MIDDLE_BACKWARDS_VICTOR_PORT,
+			RobotMap.FORWARD_ENCODER_1_PORT, RobotMap.FORWARD_ENCODE2_2_PORT,
+			RobotMap.SIDEWAYS_ENCODER_1_PORT, RobotMap.SIDEWAYS_ENCODER_2_PORT,
+			RobotMap.WHEEL_DIAMETER);
 
-    /**
+	/**
      *
      */
-    public static final Lifter lifter = new Lifter(RobotMap.LIFTER_TALON_PORT,RobotMap.LIFTER_UP_DI_PORT, RobotMap.LIFTER_DOWN_DI_PORT, RobotMap.LIFTER_ENCODER_PORT1, RobotMap.LIFTER_ENCODER_PORT2,RobotMap.LIFTER_WHEEL_DIAMETER);
-    public static final Fork fork = new Fork(RobotMap.FORK_TALON_PORT, RobotMap.FORK_OPEN_DI_PORT, RobotMap.FORK_CLOSE_DI_PORT);
-    public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
-    public static OI oi;
+	public static final Lifter lifter = new Lifter(RobotMap.LIFTER_TALON_PORT,
+			RobotMap.LIFTER_UP_DI_PORT, RobotMap.LIFTER_DOWN_DI_PORT,
+			RobotMap.LIFTER_ENCODER_PORT1, RobotMap.LIFTER_ENCODER_PORT2,
+			RobotMap.LIFTER_WHEEL_DIAMETER);
+	public static final Fork fork = new Fork(RobotMap.FORK_TALON_PORT,
+			RobotMap.FORK_OPEN_DI_PORT, RobotMap.FORK_CLOSE_DI_PORT);
+	public static OI oi;
 
-    Command autonomousCommand;
+	Command autonomousCommand;
 
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
-        oi = new OI();
-        // instantiate the command used for the autonomous period
-        autonomousCommand = new ExampleCommand();
-    }
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		oi = new OI();
+		// instantiate the command used for the autonomous period
+		autonomousCommand = new AutonomousCommand();
+	}
 
-    public void disabledPeriodic() {
-        Scheduler.getInstance().run();
-    }
+	@Override
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
 
-    public void autonomousInit() {
-        // schedule the autonomous command (example)
-        if (autonomousCommand != null) {
-            autonomousCommand.start();
-        }
-    }
+	@Override
+	public void autonomousInit() {
+		// schedule the autonomous command (example)
+		if (autonomousCommand != null) {
+			autonomousCommand.start();
+		}
+	}
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-    }
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
 
-    public void teleopInit() {
-        // This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (autonomousCommand != null) {
-            autonomousCommand.cancel();
-        }
-    }
+	@Override
+	public void teleopInit() {
+		// This makes sure that the autonomous stops running when
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		if (autonomousCommand != null) {
+			autonomousCommand.cancel();
+		}
+	}
 
-    /**
-     * This function is called when the disabled button is hit. You can use it
-     * to reset subsystems before shutting down.
-     */
-    public void disabledInit() {
+	/**
+	 * This function is called when the disabled button is hit. You can use it
+	 * to reset subsystems before shutting down.
+	 */
+	@Override
+	public void disabledInit() {
 
-    }
+	}
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-        Scheduler.getInstance().run();
-    }
+	/**
+	 * This function is called periodically during operator control
+	 */
+	@Override
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
 
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-        LiveWindow.run();
-    }
+	/**
+	 * This function is called periodically during test mode
+	 */
+	@Override
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
 }
