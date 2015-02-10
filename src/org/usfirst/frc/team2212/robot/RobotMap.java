60788cb243d6fa218e62502b05b4a7b23ea529a7
Spikes-2212 <spikes.no2212@gmail.com>
2015-02-10 15:28:33 +0200

    Fixed ports

--- src/org/usfirst/frc/team2212/robot/RobotMap.java
+++ src/org/usfirst/frc/team2212/robot/RobotMap.java
@@ -8,39 +8,41 @@ package org.usfirst.frc.team2212.robot;
  */
 public class RobotMap {
 
-	public static final int LEFT_FORWARD_VICTOR_PORT = 0;
-	public static final int LEFT_BACKWARDS_VICTOR_PORT = 0;
+	public static final int LEFT_FORWARD_VICTOR_PORT = 8;
+	public static final int LEFT_BACKWARDS_VICTOR_PORT = 9;
 	public static final int RIGHT_FORWARD_VICTOR_PORT = 0;
-	public static final int RIGHT_BACKWARDS_VICTOR_PORT = 0;
-	public static final int MIDDLE_FRONT_VICTOR_PORT = 0;
-	public static final int MIDDLE_BACKWARDS_VICTOR_PORT = 0;
-	public static final int LEFT_ENCODER_1_PORT = 0;
-	public static final int LEFT_ENCODER_2_PORT = 0;
-	public static final int RIGHT_ENCODER_1_PORT = 0;
-	public static final int RIGHT_ENCODER_2_PORT = 0;
-	public static final int FRONT_ENCODER_1_PORT = 0;
-	public static final int FRONT_ENCODER_2_PORT = 0;
-	public static final int REAR_ENCODER_1_PORT = 0;
-	public static final int REAR_ENCODER_2_PORT = 0;
+	public static final int RIGHT_BACKWARDS_VICTOR_PORT = 1;
+	public static final int MIDDLE_FRONT_VICTOR_PORT = 4;
+	public static final int MIDDLE_BACKWARDS_VICTOR_PORT = 5;
+	public static final int LEFT_ENCODER_1_PORT = 3;
+	public static final int LEFT_ENCODER_2_PORT = 4;
+	public static final int RIGHT_ENCODER_1_PORT = 7;
+	public static final int RIGHT_ENCODER_2_PORT = 8;
+	public static final int FRONT_ENCODER_1_PORT = 1;
+	public static final int FRONT_ENCODER_2_PORT = 2;
+	public static final int REAR_ENCODER_1_PORT = 5;
+	public static final int REAR_ENCODER_2_PORT = 6;
 
-	public static final double WHEEL_DIAMETER = 0;
+	public static final double WHEEL_DIAMETER = 6 * 2.54; // 6'' to cm
 	public static final double FIXED_TOLARANCE = 0;
 	public static final double TURN_TOLERANCE = 0.01;
 	public static final int ENCODER_TICKS_IN_FULL_TURN = 1;
 
-	public static final int FORK_TALON_ID = 0;
-	public static final int FORK_OPEN_DI_PORT = 0;
-	public static final int FORK_CLOSE_DI_PORT = 0;
+	public static final int FORK_TALON_ID = 3;
+	public static final int FORK_OPEN_DI_PORT = -1;
+	public static final int FORK_CLOSE_DI_PORT = -1;
 
-	public static final int LIFTER_TALON_ID = 0;
-	public static final int LIFTER_UP_DI_PORT = 0;
+	public static final int LIFTER_TALON_1_ID = 1;
+	public static final int LIFTER_TALON_2_ID = 2;
+	public static final int LIFTER_UP_DI_PORT = 9;
 	public static final int LIFTER_DOWN_DI_PORT = 0;
-	public static final int LIFTER_ENCODER_PORT1 = 0;
-	public static final int LIFTER_ENCODER_PORT2 = 0;
-	public static final double LIFTER_DOWN_SPEED = 0;
+	public static final int LIFTER_ENCODER_PORT1 = -1;
+	public static final int LIFTER_ENCODER_PORT2 = -1;
+	public static final double LIFTER_UP_SPEED = 0.1;
+	public static final double LIFTER_DOWN_SPEED = 0.1;
 	public static final double LIFTER_LIFT_A_LITTLE_TIMEOUT = 0;
 	public static final double LIFTER_LIFT_A_LITTLE_SPEED = 0;
-	public static final double LIFTER_STAY_SPEED = 0;
+	public static final double LIFTER_STAY_SPEED = 0.15;
 	public static final double LIFTER_WHEEL_DIAMETER = 0;
 	public static final int MAX_LIFTER_LEVEL = 6;
 
