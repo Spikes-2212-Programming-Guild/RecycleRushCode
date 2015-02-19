caf4684010c0f4ae644dffe452b0066fdc143d80
alongurny <alongurny@gmail.com>
2015-02-19 21:55:54 +0200

    Revert "updated drivetrain"
    This reverts commit b90883339fe520a86c4fac0bf985e4d526c5746d.

--- src/org/usfirst/frc/team2212/robot/RobotMap.java
+++ src/org/usfirst/frc/team2212/robot/RobotMap.java
@@ -24,6 +24,13 @@ public class RobotMap {
 	public static final int REAR_ENCODER_1_PORT = 4;
 	public static final int REAR_ENCODER_2_PORT = 5;
 
+	public static final double MAX_TURN_SPEED = 0.25;
+	public static final double WHEEL_DIAMETER = 6; // inches
+	public static final double FIXED_TOLARANCE = 0.5;
+	public static final double TURN_TOLERANCE = 0.01;
+	public static final int ENCODER_TICKS_IN_FULL_TURN = 360;
+	public static final double MAX_ACCY = 0.1;
+	public static final double MAX_ACCX = 0.1;
 
 	public static final int FORK_TALON_ID = 3;
 	public static final int FORK_OPEN_DI_1_PORT = 7;
@@ -31,6 +38,7 @@ public class RobotMap {
 	public static final int FORK_ENCODER_PORT1 = 10;
 	public static final int FORK_ENCODER_PORT2 = 11;
 	public static final int FORK_CLOSE_DI_PORT = 8;
+	public static final double FORK_SPEED = 0.6;
 
 	public static final int LIFTER_TALON_1_ID = 1;
 	public static final int LIFTER_TALON_2_ID = 2;
