df6da078bf6540458ca8015391602607a762e643
Spikes-2212 <spikes.no2212@gmail.com>
2015-03-08 20:09:25 +0200

    added usage of recording

--- src/org/usfirst/frc/team2212/robot/JoystickMap.java
+++ src/org/usfirst/frc/team2212/robot/JoystickMap.java
@@ -5,7 +5,8 @@
  */
 package org.usfirst.frc.team2212.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
+import macro.JoystickButtonForRecord;
+import macro.MacroJoystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
@@ -15,9 +16,9 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  */
 public class JoystickMap {
 
-	protected Joystick driverJoystick = new Joystick(0);
+	protected MacroJoystick driverJoystick = new MacroJoystick(0);
 
-	protected Joystick navJoystick = new Joystick(1);
+	protected MacroJoystick navJoystick = new MacroJoystick(1);
 
 	protected Button UP_BUTTON = new JoystickButton(navJoystick, 3);
 	protected Button DOWN_BUTTON = new JoystickButton(navJoystick, 2);
@@ -30,5 +31,8 @@ public class JoystickMap {
 
 	protected Button SLOWNESS = new JoystickButton(driverJoystick, 7);
 	protected Button NORMAL = new JoystickButton(driverJoystick, 8);
+
+	protected Button RECORDING = new JoystickButtonForRecord(driverJoystick, 12);
+	protected Button PLAY = new JoystickButtonForRecord(driverJoystick, 11);
 
 }
--- src/org/usfirst/frc/team2212/robot/OI.java
+++ src/org/usfirst/frc/team2212/robot/OI.java
@@ -7,6 +7,8 @@ import org.usfirst.frc.team2212.robot.commands.forkLifter.Close;
 import org.usfirst.frc.team2212.robot.commands.forkLifter.Down;
 import org.usfirst.frc.team2212.robot.commands.forkLifter.Open;
 import org.usfirst.frc.team2212.robot.commands.forkLifter.Up;
+import org.usfirst.frc.team2212.robot.commands.macro.Play;
+import org.usfirst.frc.team2212.robot.commands.macro.Record;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -24,6 +26,8 @@ public class OI /* GEVALD */extends JoystickMap {
 		FORWARD_BUTTON.whileHeld(new Forward());
 		SIDEWAYS_BUTTON.whileHeld(new Sideways());
 		TURN_BUTTON.whileHeld(new Turn());
+		RECORDING.whileHeld(new Record("test"));
+		PLAY.whenPressed(new Play("test"));
 		SLOWNESS.whenPressed(new Command() {
 			@Override
 			protected boolean isFinished() {
@@ -49,8 +53,25 @@ public class OI /* GEVALD */extends JoystickMap {
 		});
 	}
 
+	public void setOverride(boolean override) {
+		driverJoystick.setOverride(override);
+		navJoystick.setOverride(override);
+	}
+
 	public double getDriverY() {
 		return driverJoystick.getY();
+	}
+
+	public boolean getNavigatorButton(int i) {
+		return navJoystick.getRawButton(i);
+	}
+
+	public boolean getDriverButton(int button) {
+		return driverJoystick.getRawButton(button);
+	}
+
+	public double getNavTwist() {
+		return navJoystick.getOverrideableTwist();
 	}
 
 	public double getDriverX() {
@@ -67,6 +88,38 @@ public class OI /* GEVALD */extends JoystickMap {
 
 	public double getNavX() {
 		return navJoystick.getX();
+	}
+
+	public void setDriverButton(int button, boolean state) {
+		driverJoystick.setButton(button, state);
+	}
+
+	public void setDriverX(double value) {
+		driverJoystick.setX(value);
+	}
+
+	public void setDriverY(double value) {
+		driverJoystick.setY(value);
+	}
+
+	public void setDriverTwist(double value) {
+		driverJoystick.setTwist(value);
+	}
+
+	public void setNavigatorButton(int button, boolean state) {
+		navJoystick.setButton(button, state);
+	}
+
+	public void setNavigatorX(double value) {
+		navJoystick.setX(value);
+	}
+
+	public void setNavigatorY(double value) {
+		navJoystick.setY(value);
+	}
+
+	public void setNavigatorTwist(double value) {
+		navJoystick.setTwist(value);
 	}
 
 }
