c0869af4bac0c2a0361fd75db3afc410a37bfe33
ThinkRedstone <ittai11@gmail.com>
2015-09-24 17:34:25 +0300

    Changed wheel diameter from inches to meters

--- src/org/usfirst/frc/team2212/robot/subsystems/Lifter.java
+++ src/org/usfirst/frc/team2212/robot/subsystems/Lifter.java
@@ -20,126 +20,126 @@ import edu.wpi.first.wpilibj.tables.ITable;
  */
 public class Lifter extends PIDSubsystem {
 
-	public static final double P = 0.25;
-	public static final double I = 0;
-	public static final double D = 0;
-	public static final double TOLERANCE = 0.125;
-	// public static final double STAY_SPEED = 0.15;
-	public static final double WHEEL_DIAMETER = 4;
-	public static final int LIFTER_MAX_LEVEL = 6;
-	public static final double ENCODER_TICKS_IN_FULL_TURN = 360;
-	public static final double DISTANCE_PER_PULSE = Math.PI * WHEEL_DIAMETER
-			/ ENCODER_TICKS_IN_FULL_TURN;
-	private static final double MIN_HEIGHT = 0;
-	private static final double MAX_HEIGHT = 0;
+    public static final double P = 0.25;
+    public static final double I = 0;
+    public static final double D = 0;
+    public static final double TOLERANCE = 0.125;
+    // public static final double STAY_SPEED = 0.15;
+    public static final double WHEEL_DIAMETER = 6 * 2.54;
+    public static final int LIFTER_MAX_LEVEL = 6;
+    public static final double ENCODER_TICKS_IN_FULL_TURN = 360;
+    public static final double DISTANCE_PER_PULSE = Math.PI * WHEEL_DIAMETER
+            / ENCODER_TICKS_IN_FULL_TURN;
+    private static final double MIN_HEIGHT = 0;
+    private static final double MAX_HEIGHT = 0;
 
 	// Put methods for controlling this subsystem
-	// here. Call these from Commands.
-	CANTalon elevator1, elevator2;
-	DigitalInput up, down;
-	Encoder encoder;
-	int currentLevel;
+    // here. Call these from Commands.
+    CANTalon elevator1, elevator2;
+    DigitalInput up, down;
+    Encoder encoder;
+    int currentLevel;
 
-	public Lifter() {
-		super(P, I, D);
-		setSetpoint(0);
-		setAbsoluteTolerance(TOLERANCE);
-		setOutputRange(-1, 1);
-		this.elevator1 = new CANTalon(RobotMap.LIFTER_TALON_1);
-		this.elevator2 = new CANTalon(RobotMap.LIFTER_TALON_2);
-		this.up = new DigitalInput(RobotMap.LIFTER_UP_DI);
-		this.down = new DigitalInput(RobotMap.LIFTER_DOWN_DI);
-		this.encoder = new Encoder(RobotMap.LIFTER_ENCODER_A,
-				RobotMap.LIFTER_ENCODER_B);
-		encoder.setDistancePerPulse(DISTANCE_PER_PULSE);
-	}
+    public Lifter() {
+        super(P, I, D);
+        setSetpoint(0);
+        setAbsoluteTolerance(TOLERANCE);
+        setOutputRange(-1, 1);
+        this.elevator1 = new CANTalon(RobotMap.LIFTER_TALON_1);
+        this.elevator2 = new CANTalon(RobotMap.LIFTER_TALON_2);
+        this.up = new DigitalInput(RobotMap.LIFTER_UP_DI);
+        this.down = new DigitalInput(RobotMap.LIFTER_DOWN_DI);
+        this.encoder = new Encoder(RobotMap.LIFTER_ENCODER_A,
+                RobotMap.LIFTER_ENCODER_B);
+        encoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+    }
 
-	public void set(double s) {
-		elevator1.set(-s);
-		elevator2.set(-s);
-	}
+    public void set(double s) {
+        elevator1.set(-s);
+        elevator2.set(-s);
+    }
 
-	public boolean isUp() {
-		return up.get();
-	}
+    public boolean isUp() {
+        return up.get();
+    }
 
-	public boolean isDown() {
-		return down.get();
-	}
+    public boolean isDown() {
+        return down.get();
+    }
 
-	public double getHeight() {
-		return encoder.getDistance();
-	}
+    public double getHeight() {
+        return encoder.getDistance();
+    }
 
-	public void reset() {
-		encoder.reset();
-	}
+    public void reset() {
+        encoder.reset();
+    }
 
-	public void levelUp() {
-		// You can only level up if the level is not corrupted
-		if (currentLevel != -1) {
-			currentLevel++;
-		}
-	}
+    public void levelUp() {
+        // You can only level up if the level is not corrupted
+        if (currentLevel != -1) {
+            currentLevel++;
+        }
+    }
 
-	public void levelDown() {
-		if (currentLevel != -1) {
-			currentLevel--;
-		}
-	}
+    public void levelDown() {
+        if (currentLevel != -1) {
+            currentLevel--;
+        }
+    }
 
-	public void resetLevel() {
-		currentLevel = 0;
-	}
+    public void resetLevel() {
+        currentLevel = 0;
+    }
 
-	public void corruptLevel() {
-		currentLevel = -1;
-	}
+    public void corruptLevel() {
+        currentLevel = -1;
+    }
 
-	public void setLevel(int level) {
-		currentLevel = level;
-	}
+    public void setLevel(int level) {
+        currentLevel = level;
+    }
 
-	public int getLevel() {
-		return currentLevel;
-	}
+    public int getLevel() {
+        return currentLevel;
+    }
 
-	public void verifyLevel() {
-		if (down.get()) {
-			this.resetLevel();
-		}
-		if (up.get()) {
-			this.setLevel(LIFTER_MAX_LEVEL);
-		}
-	}
+    public void verifyLevel() {
+        if (down.get()) {
+            this.resetLevel();
+        }
+        if (up.get()) {
+            this.setLevel(LIFTER_MAX_LEVEL);
+        }
+    }
 
-	@Override
-	public void initDefaultCommand() {
-		setDefaultCommand(new PIDStay());
-	}
+    @Override
+    public void initDefaultCommand() {
+        setDefaultCommand(new PIDStay());
+    }
 
-	@Override
-	public double returnPIDInput() {
-		return getHeight();
-	}
+    @Override
+    public double returnPIDInput() {
+        return getHeight();
+    }
 
-	@Override
-	public void usePIDOutput(double output) {
-		set(output);
-	}
-	
-	@Override
-	public String getSmartDashboardType() {
-		return "Lifter";
-	}
-	
-	@Override
-	public void initTable(ITable table) {
-		super.initTable(table);
-		table.putNumber("height", getHeight());
-		table.putNumber("minHeight", MIN_HEIGHT);
-		table.putNumber("maxHeight", MAX_HEIGHT);
-		table.putBoolean("up", isUp());
-		table.putBoolean("down", isDown());
-	}
+    @Override
+    public void usePIDOutput(double output) {
+        set(output);
+    }
+
+    @Override
+    public String getSmartDashboardType() {
+        return "Lifter";
+    }
+
+    @Override
+    public void initTable(ITable table) {
+        super.initTable(table);
+        table.putNumber("height", getHeight());
+        table.putNumber("minHeight", MIN_HEIGHT);
+        table.putNumber("maxHeight", MAX_HEIGHT);
+        table.putBoolean("up", isUp());
+        table.putBoolean("down", isDown());
+    }
 }
